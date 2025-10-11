9cded851dc62ae3e8bd897002fe19b19e3aa06a7
0o3q <2045minkyu12389@gmail.com>
2025-10-11 20:19:44 +0900

    add transfer

---
+++ contracts/MyToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        _mint(_amount*10**uint256(decimals), msg.sender);
+    }
+
+    function _mint(uint256 _amount, address _owner) internal {
+        totalSupply += _amount;
+        balanceOf[_owner] += _amount;
+    }
+
+    function transfer(uint256 _amount, address _to) external {
+        require(balanceOf[msg.sender] >= _amount, "insufficient balance");
+        balanceOf[msg.sender] -= _amount;
+        balanceOf[_to] += _amount;
+    }
+}
