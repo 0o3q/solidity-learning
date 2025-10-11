198fb5a745d28ae74ff9c28881fde8165af05c5d
0o3q <2045minkyu12389@gmail.com>
2025-10-11 21:34:51 +0900

    added approve and transferFrom

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.28;
 
 contract MyToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 amount);
+
     string public name;
     string public symbol;
     uint8 public decimals;
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount) {
         name = _name;
@@ -16,14 +20,35 @@ contract MyToken {
         _mint(_amount*10**uint256(decimals), msg.sender);
     }
 
-    function _mint(uint256 _amount, address _owner) internal {
+    function approve(address _spender, uint256 _amount) external {
+        allowance[msg.sender][_spender] = _amount;
+        
+        emit Approval(_spender, _amount);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _amount) external {
+        address spender = msg.sender;
+        // require(balanceOf[_from] >= _amount, "insufficient balance");
+        require(allowance[_from][spender] >= _amount, "insufficient allowance");
+        allowance[_from][spender] -= _amount;
+        balanceOf[_from] -= _amount;
+        balanceOf[_to] += _amount;
+
+        emit Transfer(_from, _to, _amount);
+    }
+
+    function _mint(uint256 _amount, address _owner) internal { 
         totalSupply += _amount;
         balanceOf[_owner] += _amount;
+
+        emit Transfer(address(0), _owner, _amount);
     }
 
     function transfer(uint256 _amount, address _to) external {
         require(balanceOf[msg.sender] >= _amount, "insufficient balance");
         balanceOf[msg.sender] -= _amount;
         balanceOf[_to] += _amount;
-    }
+
+        emit Transfer(msg.sender, _to, _amount);
+    } 
 }
